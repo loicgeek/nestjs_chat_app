daaa67497e89d497c8bc54bcc7788e703dfe2618
loic.ngou <loic.ngou@1menu.me>
2019-11-25 09:15:58 +0100

    migrate to sqlite for deployment

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+// export const TYPEORMCONFIG: TypeOrmModuleOptions = {
+//   database: 'chat_app_server',
+//   host: 'localhost',
+//   username: 'root',
+//   port: 3306,
+//   type: 'mysql',
+//   entities: [__dirname + '/../**/*.entity{.js,.ts}'],
+//   synchronize: true,
+// };
+
 export const TYPEORMCONFIG: TypeOrmModuleOptions = {
-  database: 'chat_app_server',
-  host: 'localhost',
-  username: 'root',
-  port: 3306,
-  type: 'mysql',
+  database: '../chat_app_server.db',
+  type: 'sqlite',
   entities: [__dirname + '/../**/*.entity{.js,.ts}'],
   synchronize: true,
 };
