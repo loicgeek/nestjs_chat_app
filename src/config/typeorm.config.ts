2528f11ce2bff95f2527561e9dbc603b91751cec
loic.ngou <loic.ngou@1menu.me>
2019-11-28 10:25:41 +0100

    change database to clearDB

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -10,9 +10,30 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 //   synchronize: true,
 // };
 
+// export const TYPEORMCONFIG: TypeOrmModuleOptions = {
+//   database: 'chat_app_server.db',
+//   type: 'sqlite',
+//   entities: [__dirname + '/../**/*.entity{.js,.ts}'],
+//   synchronize: true,
+// };
+
+/*
+ClearDB config 
+*/
+
+const database = 'heroku_b6c8628db9f69b2';
+const host = 'eu-cdbr-west-02.cleardb.net';
+const username = 'b0ec56135a6d95';
+const password = 'cbd1c26e';
+const port = 3306;
+
 export const TYPEORMCONFIG: TypeOrmModuleOptions = {
-  database: 'chat_app_server.db',
-  type: 'sqlite',
+  database,
+  host,
+  username,
+  password,
+  port,
+  type: 'mysql',
   entities: [__dirname + '/../**/*.entity{.js,.ts}'],
   synchronize: true,
 };
